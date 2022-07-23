f42ac7f3b4ad3b9ad66694fb31cee1a3f7da3661
goya123 <akhilg642@gmail.com>
2022-07-23 18:58:50 +0530

    Adding Delete Contact Functionality

--- Program.cs
+++ Program.cs
@@ -14,8 +14,9 @@
                 Console.WriteLine("1 - ADD to Contact");
                 Console.WriteLine("2 - Display Contact List");
                 Console.WriteLine("3 - Edit a Contact");
-                Console.WriteLine("4 - Close the Application");
-                Console.WriteLine("5 - Continue");
+                Console.WriteLine("4 - Delete a Contact");
+                Console.WriteLine("5 - Close the Application");
+                Console.WriteLine("6 - Continue");
 
                 options = Convert.ToInt32(Console.ReadLine());
 
@@ -33,14 +34,17 @@
                         ContactPerson.EditContact(addressBook);
                         break;
                     case 4:
+                        ContactPerson.RemoveContact(addressBook);
                         break;
                     case 5:
+                        break;
+                    case 6:
                         continue;
 
                 }
 
             }
-            while (options != 4);
+            while (options != 5);
                 
             
 

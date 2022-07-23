63c227b22cfe80891bc90cc8b0ed9dd2d97f1db9
goya123 <akhilg642@gmail.com>
2022-07-23 11:54:15 +0530

    Creating class ContactPerson with required details

---
+++ ContactPerson.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Address_Book_System
+{
+    public class ContactPerson
+    {
+        public string FirstName;
+        public string LastName;
+        public string city;
+        public string address;
+        public string PhoneNumber;
+        public string State;
+        public string Zip;
+        public string Email;
+
+        
+    }
+    
+}

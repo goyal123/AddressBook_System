5daafea7ffe13e1f6c581609947b7c6d7a16a626
goya123 <akhilg642@gmail.com>
2022-07-23 12:37:37 +0530

    Inheriting class ContactPerson into AddPerson class and adding contact to list

--- ContactPerson.cs
+++ ContactPerson.cs
@@ -14,7 +14,43 @@ namespace Address_Book_System
         public string Zip;
         public string Email;
 
-        
+
     }
-    
+
+    public class AddPerson:ContactPerson
+    {
+        public static void AddContact()
+        {
+            AddPerson person = new AddPerson();     //creating object of class
+            List<AddPerson> addressBook = new List<AddPerson>();   //creating list containg objects of same class
+
+            Console.Write("Enter First Name: ");
+            person.FirstName = Console.ReadLine();
+
+            Console.Write("Enter Last Name: ");
+            person.LastName = Console.ReadLine();
+
+            Console.Write("Enter Phone Number: ");
+            person.PhoneNumber = Console.ReadLine();
+
+            Console.Write("Enter Address: ");
+            person.address = Console.ReadLine();
+
+            Console.Write("Enter City: ");
+            person.city = Console.ReadLine();
+
+            Console.Write("Enter Zip code: ");
+            person.Zip = Console.ReadLine();
+
+            Console.Write("Enter State: ");
+            person.State = Console.ReadLine();
+
+            Console.Write("Enter Email: ");
+            person.Email = Console.ReadLine();
+
+            addressBook.Add(person);   //Adding object to list
+        }
+
+    }
+
 }
